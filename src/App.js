d6b4a31a019884c60126864fb670f9fea427eda9
Ana-Anastassia <npaliakova@gmail.com>
2026-04-16 15:01:00 -0400

    Commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import "./index.css";
+import Footer from "./components/Footer";
+import React , { useEffect, useState } from "react";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books.jsx";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart";
+function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    const dupeItem = cart.find((item) => item.id === book.id);
+    if (dupeItem) {
+      setCart(
+        cart.map((item) => {
+          if (item.id === dupeItem.id) {
+            return { 
+              ...item,
+               quantity: item.quantity + 1,
+            };
+           } else {
+              return item;
+            }
+          })
+      );
+    } else {
+      setCart([...cart, { ...book, quantity: 1 }]);
+    }
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => 
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter((book) => book.id !== item.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += item.quantity;
+    });
+    return counter;
+
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart])
+
+  
+
+  return (
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" exact element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart}  />} />
+          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem} />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,86 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../components/ui/Rating';
+import Price from '../components/ui/Price';
+import Book from '../components/ui/Book';
+
+ const BookInfo = ({ books, addToCart, cart }) => {
+    const { id } = useParams();
+    const book = books.find((book) => +book.id === +id);
+    const [added, setAdded] = useState(false);
+
+    function addBookToCart(book) {
+        setAdded(true);
+        addToCart(book);
+    }
+
+    function bookExistOnCart() {
+        return cart.find(book => +book.id === +id);
+    }
+   
+    return (
+        <div id="books__body">
+            <main id="books__main">
+                <div className="books__container">
+                    <div className="row">
+                        <div className="book__seleted--top">
+                            <Link to="/books" className="book__link">
+                                <FontAwesomeIcon icon="arrow-left" />
+                            </Link>
+                            <Link to="/books" className="book__link">
+                              <h2 className="book__selected--title--top">Books</h2>
+                            </Link>
+                        </div>
+                        <div className="book__selected">
+                            <figure className="book__selected--figure">
+                                <img className="book__selected--img" 
+                                 src={book.url} 
+                                 alt="" 
+                                />
+                            </figure>
+                            <div className="book__selected--description">
+                                <h2 className="book__selected--title">{book.title}</h2>
+                                <Rating rating={book.rating} />
+                                <div className="book__selected--price">
+                                    <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+                                </div>
+                                <div className="book__summary">
+                                    <h3 className="book__summary--title">Summary</h3>
+                                    <p className="book__summary--para">
+                                        Cracking the Coding Interview is a book that helps prepare for technical interviews in software engineering companies. It contains 189 programming problems and solutions, covering topics like data structures, algorithms, and system design.
+                                    </p>
+                                    <p className="book__summary--para">
+                                        Cracking the Coding Interview is a book that helps prepare for technical interviews in software engineering companies. It contains 189 programming problems and solutions, covering topics like data structures, algorithms, and system design.
+                                    </p>
+                                </div>
+                                 <button className="btn" onClick={() => addBookToCart(book)}>
+                                        Add to cart
+                                    </button>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="books__container">
+                    <div className="row">
+                        <div className="book__seleted--top">
+                            <h2 className="book__selected--title--top">
+                                Recommended Books
+                            </h2>
+                        </div>
+                        <div className="books">
+                            {books
+                                .filter(book => book.rating === 5 && +book.id !== +id)
+                                .slice(0,4)
+                                .map(book => <Book book={book} key={book.id} />)
+                            }
+                        </div>
+
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+export default BookInfo;
