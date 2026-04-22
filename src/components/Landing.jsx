c97fd092a4981b72cf7134902dc92d9ba7a64693
Ana-Anastassia <npaliakova@gmail.com>
2026-04-21 22:44:16 -0400

    new commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
         <header>
             <div className="header__container">
                 <div className="header__description">
-                   <h1>Most awarded online library platform</h1>
+                   <h1>Most awarded online library platform.</h1>
                    <h2>Find your dream book with <span className="purple">Library</span></h2>
                    <Link to="#features">
                       <button className="btn">Browse Books</button>
